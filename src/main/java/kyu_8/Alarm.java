2f76acc3749c399b4828c96d56405c2a0e666aff
nprblm <vitalik.bella@gmail.com>
2023-05-02 14:34:52 +0300

    add solution to https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/java Kyu 8

---
+++ src/main/java/kyu_8/Alarm.java
@@ -0,0 +1,9 @@
+package kyu_8;
+//https://www.codewars.com/kata/568dcc3c7f12767a62000038/java
+
+public class Alarm {
+
+    public static boolean setAlarm(boolean employed, boolean vacation) {
+        return employed == true && vacation == false;
+    }
+}
