4107354615360e75c394adbb09df2c99cfc3c7a2
nprblm <vitalik.bella@gmail.com>
2023-05-03 21:25:03 +0300

    add solution to https://www.codewars.com/kata/56b29582461215098d00000f/train/java Kyu 8

---
+++ src/main/java/kyu_8/LarioAndMuigiPipeProblem.java
@@ -0,0 +1,10 @@
+package kyu_8;
+//https://www.codewars.com/kata/56b29582461215098d00000f/train/java
+
+import java.util.stream.IntStream;
+
+public class LarioAndMuigiPipeProblem {
+    public static int[] pipeFix(int[] numbers) {
+        return IntStream.range(numbers[0], numbers[numbers.length - 1] + 1).toArray();
+    }
+}
